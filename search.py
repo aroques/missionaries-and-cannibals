ad71eae8d49cdbb09f9495ce5db794c72f5105d5
Alec Roques <alec.roques@gmail.com>
2018-02-17 12:22:57 -0600

    Format search.py

--- search.py
+++ search.py
@@ -20,14 +20,19 @@ def recursive_dls(node, problem, limit):
     if problem.goal_is(node.state):
         print('solution found at depth {}!'.format(node.depth))
         return node
+
     elif limit == 0:
         return 'cutoff'
+
     else:
         cutoff_occurred = False
         for child in node.expand(problem):
+
             result = recursive_dls(child, problem, limit - 1)
+
             if result == 'cutoff':
                 cutoff_occurred = True
             elif result is not None:
                 return result
+
         return 'cutoff' if cutoff_occurred else None
